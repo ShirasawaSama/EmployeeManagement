cc1e537802115e2c250b2d652c2773ecab14c27e
Shirasawa <764798966@qq.com>
2023-05-18 14:16:53 +0800

    feat: build app

--- src/main/java/cn/apisium/ems/controllers/EmployeeController.java
+++ src/main/java/cn/apisium/ems/controllers/EmployeeController.java
@@ -9,6 +9,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
 
 @RestController
+@RequestMapping("/api")
 public final class EmployeeController {
     private final EmployeeService service;
 
